e1480c0fed641979e907a36c284fc4c5aedeefae
vhostbase <vhostbase@gmail.com>
2020-09-12 07:00:31 +0530

    u

--- TELEMEDICINE-APP/src/app/auth/lazyloader.routers.ts
+++
@@ -1,9 +0,0 @@
-import { RouterModule, Routes, CanActivate, CanDeactivate } from '@angular/router';
-import { AuthComponent } from './auth.component';
-import { AuthGuard } from './auth.guard';
-
-export const appRoutes: Routes = [{
-    path: '', component: AuthComponent, children: [
-        { path: 'dashboard', loadChildren: '../dashboard/dashboard.module#DashboardModule', canActivate:[AuthGuard] },
-    ]
-}];
